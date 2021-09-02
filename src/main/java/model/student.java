f43435f445c658f03ece6431cc69badcbf76447d
DESKTOP-Q8RBDDR\user <francesco.corrado@stepsrl.org>
2021-09-02 17:45:17 +0200

    some fixes

--- src/main/java/model/student.java
+++ src/main/java/model/student.java
@@ -1,9 +1,13 @@
 package model;
 
-public class student {
-private int ID;
-private String prova;
+public class Student {
+private int ID = 1;
+private String prova = "Pippo";
 
+
+public Student(int i, String string) {
+	// TODO Auto-generated constructor stub
+}
 
 public int getID() {
 	return ID;
@@ -20,4 +24,8 @@ public String getProva() {
 public void setProva(String prova) {
 	this.prova = prova;
 }
+
+public String toString(){  
+    return ID+" "+prova;  
+}  
 }
